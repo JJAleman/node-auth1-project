6f8fb3a2c441a2f07ea100f571e0a6e4907be55f
Juan Aleman <juan.j.aleman@gmail.com>
2020-02-16 20:07:35 -0600

    mvp just need to find my bug

---
+++ users/users-router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const Users = require('./users-model.js');
+
+const authRequired = require('../auth/auth-required-middleware');
+
+router.get('/', authRequired, (req, res) => {
+    Users.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch( err => {
+        res.send(err)
+    })
+})
+
+module.exports = router;
